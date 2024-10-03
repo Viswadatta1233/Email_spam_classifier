f686bd8ac6baf6d06bfa8bb5ec4af66e6b137d33
Viswadatta1233 <167094034+Viswadatta1233@users.noreply.github.com>
2024-10-03 09:56:53 +0530

    Add files via upload

---
+++ Emailspam/app.py
@@ -0,0 +1,44 @@
+import streamlit as st
+import pickle
+import string
+from nltk.corpus import stopwords
+import nltk
+from nltk.stem.porter import PorterStemmer
+ps=PorterStemmer()
+tfidf=pickle.load(open('vectorizer.pkl','rb'))
+model=pickle.load(open('model.pkl','rb'))
+st.title("Email Spam Classifier")
+input_sms=st.text_area("Enter the email message")
+
+def transform_text(text):
+    text = text.lower()
+    text = nltk.word_tokenize(text)
+
+    y = []
+    for i in text:
+        if i.isalnum():
+            y.append(i)
+
+    text = y[:]
+    y.clear()
+
+    for i in text:
+        if i not in stopwords.words('english') and i not in string.punctuation:
+            y.append(i)
+
+    text = y[:]
+    y.clear()
+
+    for i in text:
+        y.append(ps.stem(i))
+
+
+    return " ".join(y)
+if st.button('Predict'):
+    transform_sms=transform_text(input_sms)
+    vector_input=tfidf.transform([transform_sms])
+    result=model.predict(vector_input)
+    if(result==1):
+       st.header("Spam")
+    else:
+       st.header("Not spam")
